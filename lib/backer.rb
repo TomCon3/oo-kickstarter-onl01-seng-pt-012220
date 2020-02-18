cf25b1f4b09cb714d9591fa393249f6aefd3fe77
Tommy Connor <tconnor98@aol.com>
2020-02-18 17:31:53 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,7 +7,14 @@ class Backer
     @backed_projects = []
   end
   
-  def back_project
+  def back_project(project)
+    @backed_projects << project
+  end
+  
+  def add_backer(self)
+    
+    
+  end
     
   
   
